0cfbf42de8fcf8974521981db796731e54f571b4
Paulo Ribeiro <plribeiro3000@gmail.com>
2024-02-28 22:50:16 -0300

    feat(*): Initial Commit

---
+++ lib/validates_identity/cl_rut.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "cl_rut/version"
+
+module ValidatesIdentity
+  module ClRut
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ spec/validates_identity/cl_rut_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe ValidatesIdentity::ClRut do
+  it "has a version number" do
+    expect(ValidatesIdentity::ClRut::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
