f173de4e60916804145a6a488d35f63009485b34
Paulo Ribeiro <plribeiro3000@gmail.com>
2026-03-20 14:12:43 -0300

    fix(*): Chilean RUT validation for 7-digit numbers and new identity types

--- lib/validates_identity/cl_rut.rb
+++ lib/validates_identity/cl_rut.rb
@@ -11,5 +11,10 @@ end
 
 # Person and Legal
 ValidatesIdentity.register_legal_identity_type('CL_RUT', ValidatesIdentity::ClRut::Validator)
-ValidatesIdentity::ShouldaMatchers.register_legal_allowed_values('CL_RUT', %w[760864285 76.086.428-5 220604497 22.060.449-7])
-ValidatesIdentity::ShouldaMatchers.register_legal_disallowed_values('CL_RUT', %w[76086428K 76.086.428-K 220604492 22.060.449-2])
+ValidatesIdentity.register_person_identity_type('CL_RUT', ValidatesIdentity::ClRut::Validator)
+ValidatesIdentity.register_legal_identity_type_alias('CL_RUT', 'CL_RUT_LEGAL')
+ValidatesIdentity.register_person_identity_type_alias('CL_RUT', 'CL_RUT_PERSON')
+ValidatesIdentity::ShouldaMatchers.register_legal_allowed_values('CL_RUT_LEGAL', %w[760864285 76.086.428-5 220604497 22.060.449-7])
+ValidatesIdentity::ShouldaMatchers.register_legal_disallowed_values('CL_RUT_LEGAL', %w[76086428K 76.086.428-K 220604492 22.060.449-2])
+ValidatesIdentity::ShouldaMatchers.register_person_allowed_values('CL_RUT_PERSON', %w[125319092 12.531.909-2 85490923 8.549.092-3])
+ValidatesIdentity::ShouldaMatchers.register_person_disallowed_values('CL_RUT_PERSON', %w[12531909K 12.531.909-K 85490921 8.549.092-1])
